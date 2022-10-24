a9d830836dce760b807696915445f975a3b999eb
Mathias Louboutin <mathias.louboutin@gmail.com>
2022-10-24 18:47:48 -0400

    Update opm.sh

--- opm.sh
+++ opm.sh
@@ -2,7 +2,7 @@
 
 
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 
 sudo apt-add-repository ppa:opm/ppa
 sudo apt-get update
