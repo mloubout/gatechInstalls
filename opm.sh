6c8aa492a7c19ac945fd165c313900d97f6d1ee9
Mathias Louboutin <mathias.louboutin@gmail.com>
2022-10-24 18:49:03 -0400

    Update opm.sh

--- opm.sh
+++ opm.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 sudo apt-get install -y software-properties-common
 
-sudo apt-add-repository ppa:opm/ppa
+sudo apt-add-repository -y ppa:opm/ppa
 sudo apt-get update
 
 sudo apt-get install -y mpi-default-bin libopm-simulators-bin
