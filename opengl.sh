3e027dde75d196d8d709b4c3f90d5de483bed3d3
Mathias Louboutin <mathias.louboutin@gmail.com>
2022-10-28 08:37:40 -0400

    Update opengl.sh

--- opengl.sh
+++ opengl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt-get install -y libglu1-mesa-dev freeglut3-dev mesa-common-dev
+sudo apt-get install -y libglu1-mesa-dev freeglut3-dev mesa-common-dev mesa-utils
 
 sudo apt clean all
