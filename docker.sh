f7932c5480b0785b98b3a99de2d66a085154b52d
Mathias Louboutin <mathias.louboutin@gmail.com>
2023-02-06 18:37:48 -0500

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+
+# Make user avail
+sudo groupadd docker
+for u in $(users); do sudo usermod -aG docker $u; done
+
+# nvidia docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+    && curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add - \
+    && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
