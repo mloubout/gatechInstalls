f2203f13ccd9c547100025bcf5f41419410d7cb4
Mathias Louboutin <mathias.louboutin@gmail.com>
2023-02-06 18:42:29 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -15,4 +15,4 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
     && curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add - \
     && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
     
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-docker2
